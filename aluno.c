a6abcc2c573310d28203fd977f2af3713e3ea943
Antonio Neto <113268997+antonioneto2@users.noreply.github.com>
2023-03-27 19:22:56 -0300

    update

--- aluno.c
+++ aluno.c
@@ -1,5 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include "aluno.h"
+#define TAM 100
+
+struct aluno
+{
+    char nome[91];
+    int matricula;
+    int documento;
+};
+Aluno *cria_aluno(char *nome, int matricula, int documento)
+{
+    Aluno *aluno = (Aluno *)malloc(sizeof(Aluno));
+    // Verifica a alocação
+    if (aluno == NULL)
+    {
+        printf("ERRO, MEMORIA INSUFICIENTE!\n");
+        exit(1);
+    }
+}
+
+int obter_alunos(Aluno **alunos)
+{
+    FILE *arquivo_origem;
+    char linha[TAM], nome[91];
+    int matricula, documento, i = 0, linhas = 0;
+}
+
+void trocar(char **a, char **b)
+{
+    char *temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int particionar(char **arr, int baixo, int alto)
+{
+    char *pivo = arr[alto]; // escolhe o pivo como o ultimo elemento
+    int i = baixo - 1;      // indice do menor elemento
+
+    for (int j = baixo; j <= alto - 1; j++)
+    {
+        if (strcmp(arr[j], pivo) < 0)
+        {
+            i++; // incrementa o indice do menor elemento
+            trocar(&arr[i], &arr[j]);
+        }
+    }
+    trocar(&arr[i + 1], &arr[alto]);
+    return (i + 1);
+}
+
+void quick_sort(char **arr, int baixo, int alto)
+{
+    if (baixo < alto)
+    {
+        int pi = particionar(arr, baixo, alto);
+        quicksort(arr, baixo, pi - 1);
+        quicksort(arr, pi + 1, alto);
+    }
+}
--- main.c
+++ main.c
@@ -1,37 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "aluno.c"
 
-void trocar(char **a, char **b)
+int main()
 {
-    char *temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-int particionar(char **arr, int baixo, int alto)
-{
-    char *pivo = arr[alto]; // escolhe o pivo como o ultimo elemento
-    int i = baixo - 1;      // indice do menor elemento
-
-    for (int j = baixo; j <= alto - 1; j++)
-    {
-        if (strcmp(arr[j], pivo) < 0)
-        {
-            i++; // incrementa o indice do menor elemento
-            trocar(&arr[i], &arr[j]);
-        }
-    }
-    trocar(&arr[i + 1], &arr[alto]);
-    return (i + 1);
-}
-
-void quicksort(char **arr, int baixo, int alto)
-{
-    if (baixo < alto)
-    {
-        int pi = particionar(arr, baixo, alto);
-        quicksort(arr, baixo, pi - 1);
-        quicksort(arr, pi + 1, alto);
-    }
 }
