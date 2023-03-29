24fda32bd2a0b7ea5b6ad4e68f00fd1799e7f378
Antonio Neto <113268997+antonioneto2@users.noreply.github.com>
2023-03-29 16:36:41 -0300

    Update aluno.c

--- aluno.c
+++ aluno.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <time.h>
 #include "aluno.h"
-#define TAM 100
+#define TAM_LINHA 100
 
 struct aluno
 {
-    char nome[91];
+    char nome[51];
     int matricula;
     int documento;
 };
@@ -19,13 +21,56 @@ Aluno *cria_aluno(char *nome, int matricula, int documento)
         printf("ERRO, MEMORIA INSUFICIENTE!\n");
         exit(1);
     }
+    nome[0] = toupper(nome[0]);
+    strcpy(aluno->nome, nome);
+    aluno->matricula = matricula;
+    aluno->documento = documento;
+
+    printf("Aluno criado com sucesso!\n");
+    return aluno;
 }
 
 int obter_alunos(Aluno **alunos)
 {
     FILE *arquivo_origem;
-    char linha[TAM], nome[91];
+    char linha[TAM_LINHA], nome[91];
     int matricula, documento, i = 0, linhas = 0;
+    ;
+
+    arquivo_origem = fopen("dados_alunos.txt", "r"); // Abre o arquivo_origem para leitura
+    if (arquivo_origem == NULL)
+    {
+        printf("Erro ao abrir o arquivo.\n");
+        exit(1);
+    }
+    while (fgets(linha, TAM_LINHA, arquivo_origem) != NULL)
+    {
+        Aluno *aluno = (Aluno *)malloc(sizeof(Aluno));
+        sscanf(linha, " %[^;];%d;%d", nome, &matricula, &documento); // Resgata as informações do aluno
+        strcpy(aluno->nome, nome);
+        aluno->matricula = matricula;
+        aluno->documento = documento;
+        alunos[i++] = aluno; // Inclui um ponteiro de Aluno para o vetor
+        linhas++;
+    }
+    fclose(arquivo_origem); // Fecha o arquivo
+
+    return linhas;
+}
+
+void exibe_alunos(Aluno **alunos, int n_alunos)
+{
+    int i;
+    printf("----------ALUNOS----------\n");
+    printf("Nome        Matricula       Documento\n");
+    for (i = 0; i < n_alunos; i++)
+    {
+        printf("%d - %s\t\t%d\t%d\n", i + 1, alunos[i]->nome, alunos[i]->matricula, alunos[i]->documento);
+    }
+}
+void free_aluno(Aluno *aluno)
+{
+    free(aluno);
 }
 
 void trocar(char **a, char **b)
@@ -54,15 +99,18 @@ int particionar(char **arr, int baixo, int alto)
 
 void quick_sort(char **arr, int baixo, int alto)
 {
+    clock_t inicio, fim;
+    double tempo_execucao;
+
+    // Inicio da funcao quick_sort
+    inicio = clock(); // Armazena o horario de inicio da funcao
     if (baixo < alto)
     {
         int pi = particionar(arr, baixo, alto);
         quicksort(arr, baixo, pi - 1);
         quicksort(arr, pi + 1, alto);
     }
-}
-
-void free_aluno(Aluno *aluno)
-{
-    free(aluno);
+    fim = clock();                                                // Armazena o horario de termino da funcao
+    tempo_execucao = ((double)(fim - inicio)) / CLOCKS_PER_SEC;   // Calcula o tempo de execucao da funcao
+    printf("Tempo de execucao: %.2f segundos\n", tempo_execucao); // Exibindo o tempo de execucao da funcao
 }
