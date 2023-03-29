9d071b22bbc82e26bd629cd68ee262bc4ccbd7c5
Antonio Neto <113268997+antonioneto2@users.noreply.github.com>
2023-03-29 16:36:46 -0300

    Update aluno.h

--- aluno.h
+++ aluno.h
@@ -1,11 +1,1 @@
 typedef struct aluno Aluno;
-
-Aluno *cria_aluno(char *nome, int matricula, int documento);
-int obter_alunos(Aluno **alunos);
-int particionar(char **arr, int baixo, int alto);
-void trocar(char **a, char **b);
-void exibe_alunos(Aluno **alunos, int n_alunos);
-void free_aluno(Aluno *aluno);
-void quick_sort(char **arr, int baixo, int alto);
-void ordena_alunos(Aluno **alunos, int n_alunos);
-void atualiza_arquivo(Aluno **alunos, int n_alunos);
